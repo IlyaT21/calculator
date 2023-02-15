28463133ce6721a9b3bde00fc6120eb26213c230
Ilya <ilijatoskovic@gmail.com>
2023-02-16 00:42:00 +0100

    comments and filan touches

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# calculator
-Web App imitating calculator
+# Calculator Web App
+# Has four operators: + - * /
+# Buttons and operands can be added using the buttons or by typing
+# Has a history of previous inputs
+# Has a regex validator on each keyUp event in an input field
--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+
+// All processes that happen on clicking the equals button
+
 document.getElementById("equals").addEventListener("click", function () {
   
+  // Getting the last filed in an array
   let outputFields = document.querySelectorAll(".output-field");
   let lastOutputField = outputFields[outputFields.length - 1];
   let lastOutputFieldValue = lastOutputField.value;
 
+  // Divides numbers and operands in separate arrays
   if (typeof lastOutputFieldValue === "string") {
     let numbers = lastOutputFieldValue.split(/[+\-*/]/);
     let operands = lastOutputFieldValue.match(/[+\-*/]/g);
 
+    // Calculates the result
     let result = parseInt(numbers[0]);
-
     for (let i = 0; i < operands.length; i++) {
       let currentNumber = parseInt(numbers[i + 1]);
       let currentOperand = operands[i];
@@ -30,11 +35,17 @@ document.getElementById("equals").addEventListener("click", function () {
       }
     }
 
+    // Initial input remains in the field
     lastOutputField.setAttribute("value", lastOutputFieldValue);
 
+    // Disables all current fields
+    outputFields.forEach((field) => {
+      field.disabled = true;
+    });
+
+    // Adds a new field to the display and scrolls to it
     let display = document.getElementById("output-holder");
     display.innerHTML += '<input class="output-field" value="' + result + '">';
-
     display.scrollTop = display.scrollHeight;
 
   } else {
@@ -42,8 +53,8 @@ document.getElementById("equals").addEventListener("click", function () {
   }
 });
 
+// Types button value to the latest field
 let calcButtons = document.querySelectorAll('.reg-button');
-
 calcButtons.forEach((e) => {
   e.addEventListener('click', () => {
     let outputFields = document.querySelectorAll(".output-field");
@@ -51,3 +62,18 @@ calcButtons.forEach((e) => {
     lastOutputField.value += e.innerText;
   })
 })
+
+// Removes incoret input
+let allFields = document.querySelectorAll(".output-field");
+let lastField = allFields[allFields.length - 1];
+lastField.addEventListener("keyup", function() {
+  validateInput(lastField);
+});
+
+function validateInput(inputElement) {
+  let inputValue = inputElement.value;
+  let validatedValue = inputValue.replace(/[^0-9+\-*/]/g, '');
+  if (inputValue !== validatedValue) {
+    inputElement.value = validatedValue;
+  }
+}
